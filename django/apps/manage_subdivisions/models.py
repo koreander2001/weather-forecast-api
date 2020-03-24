594f6a31bfb6d9e0a842e49d9c67fd6138e2defb
koreander2001 <neokamiyama@gmail.com>
2020-03-24 09:38:04 +0900

    モデルデータの最大文字列長を修正

--- django/apps/manage_subdivisions/models.py
+++ django/apps/manage_subdivisions/models.py
@@ -4,13 +4,13 @@ from django.db import models
 class Subdivision(models.Model):
     id = models.CharField(max_length=5, primary_key=True)
     name = models.CharField(max_length=16)
-    local_name = models.CharField(max_length=8)
+    local_name = models.CharField(max_length=16)
 
 class City(models.Model):
     id = models.CharField(max_length=6, primary_key=True)
     subdivision = models.ForeignKey(Subdivision, related_name='cities', on_delete=models.CASCADE)
     name = models.CharField(max_length=64)
-    local_name = models.CharField(max_length=32)
+    local_name = models.CharField(max_length=64)
     lat = models.FloatField(validators=[MinValueValidator(-90.0), MaxValueValidator(90.0)])
     lon = models.FloatField(validators=[MinValueValidator(-180.0), MaxValueValidator(180.0)])
 
